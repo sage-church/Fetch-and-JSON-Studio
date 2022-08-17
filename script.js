5e4f4726776a62aff9c4e9c68da191415aef6dc6
sage-church <sagin.churchman77@gmail.com>
2022-08-17 17:21:13 -0500

    sorted astronauts, active astronaut text shows green, total count of astronauts shows at top of page

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
+        
+        response.json().then(function(json) {
+
+            console.log(json);
+
+            let container = document.getElementById("container");
+
+            json.sort(function(a, b) { return b.hoursInSpace - a.hoursInSpace });
+
+            container.innerHTML += `<h3>Total astronaut count: ${json.length}</h3>`
+
+            for (let i = 0; i < json.length; i++) {
+
+                if (json[i].active) {
+
+                    container.innerHTML += 
+                    `<div class="astronaut">
+                        <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li style="color:green">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(", ")}</li>
+                        </ul>
+                        </div>
+                        <img class="avatar" src=${json[i].picture}>
+                    </div>`
+
+                } else {
+
+                    container.innerHTML += 
+                    `<div class="astronaut">
+                        <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li id="isActive">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(", ")}</li>
+                        </ul>
+                        </div>
+                        <img class="avatar" src=${json[i].picture}>
+                    </div>`
+
+                }
+
+            }
+
+        });
+
+    });
+
+});
